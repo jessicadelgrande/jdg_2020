7444412b393614ef0bc48c9aebd49ba6881fa478
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-12-01 00:00:17 -0500

    starting some scroll animations

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,4 +1,42 @@
+'use strict';
 
+$(document).ready(function () {
+	//HAMBURGER MENU
+	$('#hamburgerIcon').click(function (e) {
+		//hamburgerIcon animation
+		$(this).toggleClass('open');
+
+		//toggle header nav links
+		e.stopPropagation();
+		$('.navLinks').toggleClass('show');
+	});
+
+	// close the menu when clicking anywhere else on the website
+	$('html').click(function () {
+		if ($('.navLinks').hasClass('show')) {
+			$('#hamburgerIcon, .navLinks').removeClass('open show');
+		};
+	});
+
+	//SMOOTH SCROLLS
+	$('a[href*="#"]:not([href="#"])').click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top - 105
+				}, 500);
+				return false;
+			}
+		}
+	});
+
+	// SCROLL REVEAL
+	window.sr = ScrollReveal();
+	sr.reveal('.revealing', { duration: 2000 });
+	new WOW().init();
+});
 
 
 App.init = () => {
